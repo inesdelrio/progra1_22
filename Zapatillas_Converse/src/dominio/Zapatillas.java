28c7fd2303d271ac43f8706d90978c7b9ad39a41
Nicolas Lopez Pizza <nicolas.lopezpizza@usp.ceu.es>
2022-11-16 03:58:36 -0500

    Trabajando en los Ficheros.java,

--- Zapatillas_Converse/src/dominio/Zapatillas.java
+++ Zapatillas_Converse/src/dominio/Zapatillas.java
@@ -1,2 +1,18 @@
+package dominio;
 
-la 
+public class Zapatillas 
+{ 
+	private String modelo; 
+	private String color; 
+	private int precio;
+	private int talla; 
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo; 
+	}
+
+	public String getModelo() { 
+		return modelo; 
+	}
+
+	public void set
