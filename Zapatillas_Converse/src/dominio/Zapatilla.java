d0c377b2431fa7275eb37c09b4332f59b2d4c2a5
Inés del Río <ines.delriogarcia@usp.ceu.es>
2022-11-28 11:20:50 +0100

    Zapatilla.java completo

--- Zapatillas_Converse/src/dominio/Zapatilla.java
+++ Zapatillas_Converse/src/dominio/Zapatilla.java
@@ -4,13 +4,24 @@ public class Zapatilla
 { 
 	private String modelo; 
 	private String color; 
-	private int precio;
-	private int talla; 
+	private int precio = 0;
+	private int talla = 0;
+
+
+
+	public Zapatilla(){
+	}
+
+	public Zapatilla (String modelo, String color, int precio, int talla){
+		this.modelo = modelo; 
+		this.color = color; 
+		this.precio = precio; 
+		this.talla = talla; 
+	}
 
 	public void setModelo(String modelo) {
 		this.modelo = modelo; 
 	}
-
 	public String getModelo() { 
 		return modelo; 
 	}
@@ -23,14 +34,28 @@ public class Zapatilla
 		return color; 
 	}
 
-	//public
-	//
+	
+	public void setPrecio(int precio){
+		this.precio = precio; 
+	}
+	public int getPrecio(){
+		return this.precio; 
+	}
+
+	
+	public void setTalla(int talla){
+		this.talla = talla; 
+	}
+	public int getTalla(){
+		return this.talla; 
+	}
+
 	
 
 	@Override
 	public String toString(){
-		return "modelo: " + getModelo() + " " + 
-			"color: " + getColor() + " " +
-			//"precio: " +
+		return ("El modelo de las zapatillas es: " + this.modelo + ", el color es " + this.color + ", su precio es " + this.color + " y su talla es la " + this.talla + "." ); 
 	}
+}
+
 
